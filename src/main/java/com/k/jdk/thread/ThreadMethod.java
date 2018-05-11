0c887ad657701bcca3c47ef4fef7be6faccd8bf6
1061235166@qq.com <daodishuishiadmin499be>
2018-05-11 23:10:05 +0800

    更新注释

--- src/main/java/com/k/jdk/thread/ThreadMethod.java
+++ src/main/java/com/k/jdk/thread/ThreadMethod.java
@@ -10,6 +10,7 @@ public class ThreadMethod {
     public static void yeild(){
         new Thread(()->{
             System.out.println("1 s");
+            //此处该线程会出让cpu时间片让其他线程执行，等待本线程再次获取cpu时间片后
             Thread.yield();
             System.out.println("1 c");
         }).start();
