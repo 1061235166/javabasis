dc6dfa90b49ec4004099e6613beae65f65c879ca
1061235166@qq.com <daodishuishiadmin499be>
2018-05-27 20:47:56 +0800

    增加希尔排序

---
+++ src/main/java/com/k/sort/ShellSort.java
@@ -0,0 +1,26 @@
+package com.k.sort;
+
+/**
+ * Created by k on 2018/5/27.
+ */
+public class ShellSort extends BaseSwap {
+
+    public static void main(String[] args) {
+        int[] ints = randomArray();
+        System.out.println(toString(ints));
+        sort(ints);
+        System.out.println(toString(ints));
+    }
+
+    public static void sort(int[]ints){
+        int gap = ints.length/3;
+        while (gap>=1){
+            for(int i=gap;i<ints.length;i=i+gap){
+                for(int j=i;j>=gap && ints[j]<ints[j-gap];j-=gap){
+                    swap(ints,j,j-gap);
+                }
+            }
+            gap =gap/3;
+        }
+    }
+}
