e3fc6746d124cdc8581dd062c631822e1885df7a
1061235166@qq.com <daodishuishiadmin499be>
2018-05-27 15:28:42 +0800

    增加插入排序

--- src/main/java/com/k/sort/BaseSwap.java
+++ src/main/java/com/k/sort/BaseSwap.java
@@ -7,7 +7,8 @@ public class BaseSwap {
 
     public static int[]random
             =new int[]{321,743,23,54,4567,565,5,23,4,235,54,345,9650,219};
-
+    public static int[]random2
+            =new int[]{511,456,56,245,34,467,6,78,32,54,67,891,12,567,67858};
     /**
      * 交换数组中下标a和b的元素
      * @param ints
@@ -15,9 +16,15 @@ public class BaseSwap {
      * @param b
      */
     public static void swap(int [] ints,int a,int b){
-        int v1= ints[a];
-        int v2= ints[b];
-        ints[a]=v2;
-        ints[b]=v1;
+        try {
+            int v1= ints[a];
+            int v2= ints[b];
+            ints[a]=v2;
+            ints[b]=v1;
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("indexa="+a+"indexb="+b);
+            throw new RuntimeException("indexa="+a+"indexb="+b);
+        }
+
     }
 }
--- src/main/java/com/k/sort/InsertSort.java
+++ src/main/java/com/k/sort/InsertSort.java
@@ -1,11 +1,30 @@
 package com.k.sort;
 
+import java.util.Arrays;
+
 /**
  * 插入排序
- * Created by k on 2018/5/25.
+ * 从集合头部开始，下标每向右移动一位，下标左边的元素总是会互相比较保证有序，等走到集合最右边，
+ * 所有元素都是有序的了
+ * Created by k on 2018/5/27.
  */
-public class InsertSort {
-    public static void main(String[] args) {
+public class InsertSort extends BaseSwap{
 
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(random2));
+        sort(random2);
+        System.out.println(Arrays.toString(random2));
+    }
+
+    public static void sort(int [] ints){
+        for(int i=0;i<ints.length;i++){
+            for(int j=i;j<ints.length;j++){
+                int a = ints[i];
+                int b = ints[j];
+                if(a>b){
+                    swap(ints,i,j);
+                }
+            }
+        }
     }
 }
