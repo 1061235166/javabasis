b61be1f2639babf2555a318d90b94effbb10b454
1061235166@qq.com <daodishuishiadmin499be>
2018-05-27 14:49:03 +0800

    增加注释和新方法

--- src/main/java/com/k/sort/BaseSwap.java
+++ src/main/java/com/k/sort/BaseSwap.java
@@ -4,6 +4,10 @@ package com.k.sort;
  * Created by k on 2018/5/25.
  */
 public class BaseSwap {
+
+    public static int[]random
+            =new int[]{321,743,23,54,4567,565,5,23,4,235,54,345,9650,219};
+
     /**
      * 交换数组中下标a和b的元素
      * @param ints
--- src/main/java/com/k/sort/BubbleSort.java
+++ src/main/java/com/k/sort/BubbleSort.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 /**
  * 冒泡排序
- * 每次第一个元素和他右边的元素相比较
+ * 每次第一个元素和他右边的元素相比较，如果左边比右边打，就交换位置，第一轮过后最大元素就在最右边了
+ * 依次执行，每轮过后剩余的最大元素都会靠右边
  * Created by k on 2018/5/25.
  */
 public class BubbleSort extends BaseSwap{
