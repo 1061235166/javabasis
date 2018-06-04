17b3d1ffb7cac7adfe3c30f316a4c400bd92889c
1061235166@qq.com <daodishuishiadmin499be>
2018-06-04 23:23:52 +0800

    增加二进制操作

---
+++ src/main/java/com/k/binary/BinaryOperation.java
@@ -0,0 +1,20 @@
+package com.k.binary;
+
+/**
+ * Created by k on 2018/6/4.
+ */
+public class BinaryOperation {
+
+    public static void main(String[] args) {
+        a();
+    }
+
+    public static void a(){
+        System.out.println(1|2);
+        System.out.println(Integer.toBinaryString(1));
+        System.out.println(Integer.toBinaryString(2));
+        System.out.println(Integer.toString(218,2));
+        System.out.println(Integer.toString(2,2));
+    }
+
+}
