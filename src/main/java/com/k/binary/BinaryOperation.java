5a7fe791448a3096c3e7811907e2d5c1448ef5b2
wangyunkai <wangyk@shuaiche.com>
2018-06-22 14:44:51 +0800

    修改二进制操作

--- src/main/java/com/k/binary/BinaryOperation.java
+++ src/main/java/com/k/binary/BinaryOperation.java
@@ -5,16 +5,51 @@ package com.k.binary;
  */
 public class BinaryOperation {
 
-    public static void main(String[] args) {
-        a();
-    }
+	public static void main(String[] args) {
+		operationAND();
+		System.out.println("====");
+		operationOR();
+	}
+	/**
+	 * 按位与，两个二进制数字进行对比，从最高位到最低位一一对应，如果对应的位的值都是1，结果就是1，否则是0
+	 */
+	public static void operationAND(){
+		String s = Integer.toBinaryString(100);
+		String s1 = Integer.toBinaryString(5001);
+		System.out.println(s);
+		System.out.println(s1);
+		int r = 100 & 5001;
+		System.out.println(r);
+		System.out.println(Integer.toBinaryString(r));
+	}
 
-    public static void a(){
-        System.out.println(1|2);
-        System.out.println(Integer.toBinaryString(1));
-        System.out.println(Integer.toBinaryString(2));
-        System.out.println(Integer.toString(218,2));
-        System.out.println(Integer.toString(2,2));
-    }
+	/**
+	 * 按位或，两个二进制数字进行对比，从最高位到最低位一一对应，如果对应的位只要有一个为1，结果就是1，否则是0
+	 */
+	public static void operationOR(){
+		String s = Integer.toBinaryString(100);
+		String s1 = Integer.toBinaryString(5001);
+		System.out.println(s);
+		System.out.println(s1);
+		int r = 100 | 5001;
+		System.out.println(r);
+		System.out.println(Integer.toBinaryString(r));
+	}
 
+	/**
+	 * 取反，每位值位1取0，为0取1
+	 */
+	public static void bitInversion(){
+		String s = Integer.toBinaryString(321);
+		System.out.println(s);
+		int r = (~321);
+		System.out.println(Integer.toBinaryString(r));
+	}
+
+	/**
+	 * 按位异或
+	 */
+	public static void operationXOR(){
+
+	}
 }
