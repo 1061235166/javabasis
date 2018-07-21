7a0e99c643692cb316fe6fb6921b8f8ce0afe4ab
1061235166@qq.com <daodishuishiadmin499be>
2018-07-21 16:38:04 +0800

    选择排序增加注释

--- src/main/java/com/k/sort/SelectSort.java
+++ src/main/java/com/k/sort/SelectSort.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
  * 选择排序
  * 从所有元素中找到最小（最大）的元素，放在第一个（最后一个）位置，然后继续在剩下的元素循环以上操作，
  * 到最后，所有元素都是有序的了
+ * 不稳定排序
  * Created by k on 2018/5/25.
  */
 public class SelectSort extends BaseSwap{
