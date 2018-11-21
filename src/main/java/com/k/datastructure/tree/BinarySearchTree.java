7726d9b5c51b5cd3b52385312d2ac50b2a9b4556
1061235166@qq.com <daodishuishiadmin499be>
2018-11-21 23:17:37 +0800

    新增二叉搜索树

---
+++ src/main/java/com/k/datastructure/tree/BinarySearchTree.java
@@ -0,0 +1,138 @@
+package com.k.datastructure.tree;
+
+/**
+ * 二叉搜索树
+ * Created by k on 2018/5/20.
+ */
+public class BinarySearchTree<T extends Comparable<T>> {
+
+    private Node<T>root;
+
+    private int size;
+
+    public void insert(T data) {
+        if (root == null) {
+            root = new Node<>(data, null, null, null);
+            size++;
+            return;
+        }
+        Node<T>current = root;
+        Node<T>parent = current;
+        boolean isLeft=true;
+        while (current!=null){
+            int i = current.data.compareTo(data);
+            if (i==0){
+                return;
+            }
+            if(i>0){
+                parent = current;
+                current = current.getRight();
+                isLeft =false;
+                continue;
+            }
+            if(i<0){
+                parent = current;
+                current = current.getLeft();
+                continue;
+            }
+        }
+        Node<T>newNode = new Node<>(data,parent,null,null);
+        size++;
+        if(isLeft){
+            parent.setLeft(newNode);
+        }else {
+            parent.setRight(newNode);
+        }
+    }
+
+
+
+    /**
+     * 获取节点
+     * @param data
+     * @return
+     */
+    public Node<T>get(T data){
+        return get(root,data);
+    }
+
+    private Node<T>get(Node<T>node,T data){
+        Node<T> n = node;
+        while (n != null) {
+            if(n.data.compareTo(data)>0){
+                n = n.getRight();
+                continue;
+            }
+            if(n.data.compareTo(data)<0){
+                n =n.getLeft();
+                continue;
+            }
+            break;
+        }
+        return n;
+    }
+
+    private Node<T>newNode(T data,Node<T>parent,Node<T>left,Node<T>right){
+        return new Node<>(data,parent,left,right);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree<Integer>tree = new BinarySearchTree<>();
+
+        tree.insert(1);
+
+        tree.insert(2);
+        tree.insert(99);
+        tree.insert(123);
+        tree.insert(23);
+        tree.insert(231123);
+
+        System.out.println(tree.size);
+    }
+
+    static class Node<T> {
+        private T data;
+        private Node<T>parent;
+        private Node<T>left;
+        private Node<T>right;
+
+        public Node(T data, Node<T> parent, Node<T> left, Node<T> right) {
+            this.data = data;
+            this.parent = parent;
+            this.left = left;
+            this.right = right;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getParent() {
+            return parent;
+        }
+
+        public void setParent(Node<T> parent) {
+            this.parent = parent;
+        }
+
+        public Node<T> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<T> left) {
+            this.left = left;
+        }
+
+        public Node<T> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<T> right) {
+            this.right = right;
+        }
+    }
+}
--- src/main/java/com/k/datastructure/tree/InsertBinaryTree.java
+++
@@ -1,7 +0,0 @@
-package com.k.datastructure.tree;
-
-/**
- * Created by k on 2018/5/20.
- */
-public class InsertBinaryTree {
-}
