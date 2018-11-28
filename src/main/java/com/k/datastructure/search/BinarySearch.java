b75ff48500498f1a3c9558000d20ea219138d618
wangyunkai <wangyk@shuaiche.com>
2018-11-28 15:42:20 +0800

    增加二分查找查询

--- src/main/java/com/k/datastructure/search/BinarySearch.java
+++ src/main/java/com/k/datastructure/search/BinarySearch.java
@@ -1,25 +1,30 @@
 package com.k.datastructure.search;
 
 /**
- * 二分查找，前提这个二叉树是个二叉搜索树
+ * 二分查找，查找一个有序的数组中某个数的下标
  * Created by k on 2018/5/20.
  */
 public class BinarySearch {
 
     public static void main(String[] args) {
-
+		int [] array = new int[]{1,3,4,5,6,7,8,11,14,16};
+		System.out.println(search(array,3,0,array.length-1));
     }
 
-    public static Tree<Integer> search(int e,Tree<Integer> tree){
-        while (tree != null){
-            if(tree.getData().compareTo(e)>0){
-                return search(e,tree.getLeft());
-            }else if(tree.getData().compareTo(e)<0){
-                return search(e,tree.getRight());
-            }else {
-                return tree;
-            }
-        }
-        return null;
-    }
+    public static int search(int [] array,int num,int s, int e){
+    	//不能用while
+		if (e>=s){
+			int m = (e+s)/2;
+			int r = array[m];
+			System.out.println("m:"+m+",r:"+r);
+			if(r==num){
+				return m;
+			}else if(r>num){
+				return search(array,num,s,m-1);
+			}else if(r<num){
+				return search(array,num,m+1,e);
+			}
+		}
+    	return -1;
+	}
 }
