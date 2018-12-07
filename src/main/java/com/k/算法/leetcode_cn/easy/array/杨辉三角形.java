4f0f66116d47debeff264632a2c4ad251b0ce938
wangyunkai <wangyk@shuaiche.com>
2018-12-07 10:26:45 +0800

    自己解决杨辉三角形

--- src/main/java/com/k/算法/leetcode_cn/easy/array/杨辉三角形.java
+++ src/main/java/com/k/算法/leetcode_cn/easy/array/杨辉三角形.java
@@ -2,17 +2,18 @@ package com.k.算法.leetcode_cn.easy.array;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.LongSummaryStatistics;
 
 /**
  * https://leetcode-cn.com/explore/learn/card/array-and-string/199/introduction-to-2d-array/776/
  * 输入: 5
  输出:
  [
- 		 [1],
- 		[1,1],
- 	  [1,2,1],
- 	 [1,3,3,1],
- 	[1,4,6,4,1]
+ 		 [1], 1
+ 		[1,1], 2
+ 	  [1,2,1], 3
+ 	 [1,3,3,1], 4
+ 	[1,4,6,4,1] 5
  ]
  * created by wyk
  * 2018/12/6 17:40
@@ -20,37 +21,50 @@ import java.util.List;
 public class 杨辉三角形 {
 
 	public static void main(String[] args) {
-		System.out.println(generate(3));
+		List<List<Integer>> generate = generate(16);
+		generate.forEach((list)->{
+			System.out.print("[");
+			list.forEach((i)->{
+				System.out.print(i+",");
+			});
+			System.out.print("]\n");
+		});
 	}
 
+	/**
+	 * 自己的解法
+	 * @param numRows
+	 * @return
+	 */
 	public static List<List<Integer>> generate(int numRows) {
-		List<List<Integer>>result = new ArrayList();
-		int dec = 1;
-		while(dec<=numRows){
-			List<Integer> list=new ArrayList(dec);
-			if(dec==1){
+		List<List<Integer>>result = new ArrayList<>();
+		for(int i=1;i<=numRows;i++){
+			List<Integer>list = new ArrayList<>();
+			int size = i;
+			int index = 0;
+			if(i==1){
 				list.add(1);
-				dec++;
 				result.add(list);
 				continue;
 			}
-			if(dec==2){
+			if(i==2){
 				list.add(1);
 				list.add(1);
-				dec++;
 				result.add(list);
 				continue;
 			}
-			//获取上一行的元素
-			List<Integer> preList = result.get(dec - 1-1);
-			int s = 0;
-			int sum=0;
-			for(int i=0;i<preList.size();i++){
-				sum+=i;
-				list.add(sum);
+			List<Integer> pre = result.get(result.size() - 1);
+			for(int j=1;j<=size;j++){
+				if(j==1 || j==size){
+					list.add(1);
+					continue;
+				}
+				Integer f = pre.get(index);
+				Integer s = pre.get(index + 1);
+				index++;
+				list.add(f+s);
 			}
-			list.add(1);
-			dec++;
+			result.add(list);
 		}
 		return result;
 	}
