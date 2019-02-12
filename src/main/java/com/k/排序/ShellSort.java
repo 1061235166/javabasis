3a57335a19e2788fea917ebc96ad47f7bd59d0d3
1061235166@qq.com <daodishuishiadmin499be>
2019-02-12 22:59:07 +0800

    改名

---
+++ src/main/java/com/k/排序/ShellSort.java
@@ -0,0 +1,43 @@
+package com.k.排序;
+
+import com.k.排序.swap.BaseSwap;
+
+/**
+ * 希尔排序，把一个数组按照固定间隔分开，对每个分开的数组进行插入排序
+ * Created by k on 2018/5/27.
+ */
+public class ShellSort extends BaseSwap {
+
+    public static void main(String[] args) {
+        int[] ints = randomArray();
+        System.out.println(toString(ints));
+        sort3(ints);
+        System.out.println(toString(ints));
+    }
+
+    public static void sort(int[]ints){
+        int gap = ints.length/3;
+        while (gap>=1){
+            for(int i=gap;i<ints.length;i=i+gap){
+                for(int j=i;j>=gap && ints[j]<ints[j-gap];j-=gap){
+                    swap(ints,j,j-gap);
+                }
+            }
+            gap =gap/3;
+        }
+    }
+
+    public static void sort3(int [] ints){
+        for(int i=ints.length-1;i>=0;i--){
+            int maxIndex = i;
+            for(int j=0;j<i;j++){
+                int a = ints[maxIndex];
+                int b = ints[j];
+                if(a<b){
+                    maxIndex = j;
+                }
+            }
+            swap(ints,maxIndex,i);
+        }
+    }
+}
--- src/main/java/com/k/排序/选择排序.java
+++ src/main/java/com/k/排序/选择排序.java
@@ -2,42 +2,35 @@ package com.k.排序;
 
 import com.k.排序.swap.BaseSwap;
 
+import java.util.Arrays;
+
 /**
- * 希尔排序，把一个数组按照固定间隔分开，对每个分开的数组进行插入排序
- * Created by k on 2018/5/27.
+ * 选择排序
+ * 从所有元素中找到最小（最大）的元素，放在第一个（最后一个）位置，然后继续在剩下的元素循环以上操作，
+ * 到最后，所有元素都是有序的了
+ * 不稳定排序
+ * Created by k on 2018/5/25.
  */
 public class 选择排序 extends BaseSwap {
 
-    public static void main(String[] args) {
-        int[] ints = randomArray();
-        System.out.println(toString(ints));
-        sort3(ints);
-        System.out.println(toString(ints));
-    }
-
-    public static void sort(int[]ints){
-        int gap = ints.length/3;
-        while (gap>=1){
-            for(int i=gap;i<ints.length;i=i+gap){
-                for(int j=i;j>=gap && ints[j]<ints[j-gap];j-=gap){
-                    swap(ints,j,j-gap);
-                }
-            }
-            gap =gap/3;
-        }
-    }
-
-    public static void sort3(int [] ints){
-        for(int i=ints.length-1;i>=0;i--){
-            int maxIndex = i;
-            for(int j=0;j<i;j++){
-                int a = ints[maxIndex];
+    public static void sort(int [] ints){
+        for(int i=0;i<ints.length;i++){
+            int min = i;
+            for(int j=i+1;j<ints.length;j++){
+                int a = ints[min];
                 int b = ints[j];
-                if(a<b){
-                    maxIndex = j;
+                if(a>b){
+                    min = j;
                 }
             }
-            swap(ints,maxIndex,i);
+            swap(ints,i,min);
         }
+    }
+
+    public static void main(String[] args) {
+        int [] ints = {3,4,6,7,2,3,546,56,812,3,6,7,6};
+        System.out.println(Arrays.toString(ints));
+        sort(ints);
+        System.out.println(Arrays.toString(ints));
     }
 }
