82742762cf67d80d19517f846e3955d48262a766
1061235166@qq.com <daodishuishiadmin499be>
2019-07-17 23:25:55 +0800

    改名

--- src/main/java/com/k/排序/快速排序.java
+++ src/main/java/com/k/排序/快速排序.java
@@ -5,7 +5,8 @@ import com.k.排序.swap.BaseSwap;
 /**
  * Created by k on 2018/5/27.
  */
-public class QuickSort extends BaseSwap {
+public class 快速排序 extends BaseSwap {
+
     public static void main(String[] args) {
 
     }
