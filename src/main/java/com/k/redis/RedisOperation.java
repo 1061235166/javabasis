cd6c94f74e1d9f1a6681ed3a4f9be176b9955588
wangyunkai <wangyk@shuaiche.com>
2019-08-16 16:32:20 +0800

    修改中断条件

--- src/main/java/com/k/redis/RedisOperation.java
+++ src/main/java/com/k/redis/RedisOperation.java
@@ -17,6 +17,7 @@ public class RedisOperation {
 		scan();
 		System.out.println("==================================");
 		keys();
+		sscan();
 	}
 
 	private static Jedis getJedis(){
@@ -30,17 +31,39 @@ public class RedisOperation {
 		Jedis jedis = getJedis();
 		ScanParams scanParams = new ScanParams();
 		scanParams.match("usersession*");
-		scanParams.count(1);//每次查询1个
+		scanParams.count(2);//每次查询1个
 		String cursor = ScanParams.SCAN_POINTER_START;
 		while (true){
 			ScanResult<String> scan = jedis.scan(cursor ,scanParams);
 			cursor = scan.getStringCursor();
+      		System.out.println(cursor);
 			//当指针为0的时候说明查询不到其他数据了
+      		System.out.println(scan.getResult());
 			if(cursor.equals("0")){
 				return;
 			}
-      		System.out.println(scan.getResult());
 		}
+	}
+
+	public static void sscan(){
+		Jedis jedis = getJedis();
+		String c = ScanParams.SCAN_POINTER_START;
+		ScanParams scanParams = new ScanParams();
+		scanParams.count(12);
+		scanParams.match("*");
+		int sum = 0;
+		while (true){
+			ScanResult<String> sscan = jedis.sscan("set", c, scanParams);
+			String stringCursor = sscan.getStringCursor();
+//			System.out.println(stringCursor);
+			System.out.println(sscan.getResult());
+			sum+=sscan.getResult().size();
+			c = stringCursor;
+			if(stringCursor.equals("0")){
+				break;
+			}
+		}
+   		System.out.println("sum:"+sum);
 	}
 
 	public static void keys() {
