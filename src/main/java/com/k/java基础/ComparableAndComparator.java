aafefe3c25f3b145bc011f71722b82a08efeb901
1061235166@qq.com <daodishuishiadmin499be>
2020-11-13 17:45:39 +0800

    1

---
+++ src/main/java/com/k/java基础/ComparableAndComparator.java
@@ -0,0 +1,9 @@
+package com.k.java基础;
+
+/**
+ * 区别：Comparable是排序接口，定义在对象内部的
+ * Comparator是比较接口，一般不定义在对象内部，在不能修改对象排序方法的时候使用。比如list.sort等静态方法会需要传一个Comparator接口用于实现比较
+ * Created by k on 2020/11/11.
+ */
+public class ComparableAndComparator {
+}
